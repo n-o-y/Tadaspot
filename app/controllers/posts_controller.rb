8b09a9da20de17e49e7efac1cfd4193f32bc080d
EC2 Default User <ec2-user@ip-172-31-10-46.ap-northeast-1.compute.internal>
2021-12-02 13:31:57 +0000

    [update]投稿機能を修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,11 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
   def new
     @post = Post.new
   end
+
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -14,15 +16,20 @@ class PostsController < ApplicationController
       render root_path
     end
   end
+
   def show
     @post = Post.find(params[:id])
   end
+
   def edit
   end
+
   def update
   end
+
   def destroy
   end
+
   private
   def post_params
     params.require(:post).permit(:user_id, :image_id, :name, :introduction, :latitude, :longitude, :star)
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_many :likes
+
+  geocoded_by :name
+  before_validation :geocode
+
   def liked_by?(user)
     likes.where(user_id: user.id).exists?
   end
