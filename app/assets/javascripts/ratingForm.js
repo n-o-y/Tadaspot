d5eecf016715696b1a548fef58aff32e6c2ce19c
EC2 Default User <ec2-user@ip-172-31-10-46.ap-northeast-1.compute.internal>
2022-01-21 09:31:30 +0000

    [update]新規投稿ページ,投稿編集ページのレビュー入力フォームのスクリプトを外部ファイル化

---
+++ app/assets/javascripts/ratingForm.js
@@ -0,0 +1,8 @@
+/*global $*/
+
+$('#rating-form').raty({
+        starOn: "/assets/star-on.png",
+        starOff: "/assets/star-off.png",
+        starHalf: "/assets/star-half.png",
+        scoreName: 'post[star]'
+});
