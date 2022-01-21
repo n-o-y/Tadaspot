86e7ff167aea0be1c7c4d9fb0dbb7ca64fba0821
EC2 Default User <ec2-user@ip-172-31-10-46.ap-northeast-1.compute.internal>
2022-01-21 09:24:54 +0000

    [update]投稿詳細ページ,マイページのレビューのスクリプトを外部ファイル化

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -8,3 +8,13 @@ $('.post-star').raty({
     starOff: "/assets/star-off.png",
     starHalf: "/assets/star-half.png"
 });
+
+$('.like-star').raty({
+        readOnly: true,
+        score: function(){
+          return $(this).attr('data-score');
+        },
+        starOn: "/assets/star-on.png",
+        starOff: "/assets/star-off.png",
+        starHalf: "/assets/star-half.png"
+});
